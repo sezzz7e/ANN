b44d6a358eb53b626312898bc837a8694d72c388
sezzz7e <csfein3@gmail.com>
2026-05-09 11:20:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load model
-with open("/diabetes_model.pkl", "rb") as f:
+with open("diabetes_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 # Load scaler
